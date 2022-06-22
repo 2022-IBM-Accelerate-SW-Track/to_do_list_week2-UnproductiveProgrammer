b3fd3df1fb4cc262559f38a57596f51d512268ed
vicki <vyang492@gmail.com>
2022-06-22 19:54:42 -0400

    finished lab

--- src/component/todos.js
+++ src/component/todos.js
@@ -9,7 +9,7 @@ import { Card, CardContent, Grid, ListItemButton, ListItemText, Checkbox } from 
 // 3. The map function is called to assign each array item with a key
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
@@ -21,7 +21,7 @@ const Todos = ({ todos }) => {
               <span style={{ padding: "50px" }}>{todo.content}</span>
             </CardContent> */}
             <ListItemButton component="a" href="#simple-list">
-              <Checkbox style={{ paddingLeft: 0 }} color="primary">
+              <Checkbox style={{ paddingLeft: 0 }} color="primary" onClick={() => deleteTodo(todo.id)}>
 
               </Checkbox>
               <ListItemText primary={todo.content} secondary={todo.date} />
